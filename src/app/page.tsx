cc1db7239d9826924447fdba7cd7515c4a73d0d6
jjribeiro1 <jvsr.99@hotmail.com>
2023-12-14 20:55:20 -0300

    Home container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import About from '@/components/About/About'
+import TechSection from '@/components/TechStack/TechStack'
+
 export default function Home() {
   return (
-    <div>home</div>
+    <>
+      <main className="container mx-auto my-0">
+        <About />
+        <TechSection />
+      </main>
+    </>
   )
 }
