51678905020900a66bb8c2d5c1ef63dcb1651743
jjribeiro1 <jvsr.99@hotmail.com>
2023-12-16 23:18:01 -0300

    add contact section to home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
-import Header from '@/components/Header/Header'
 
 export const metadata: Metadata = {
   title: 'jjribeirodev',
@@ -17,12 +16,9 @@ export default function RootLayout({
   return (
     <html
       lang="pt-BR"
-      className={`${GeistSans.variable} ${GeistMono.variable} bg-slate-950 scroll-smooth`}
+      className={`${GeistSans.variable} ${GeistMono.variable} scroll-smooth bg-slate-950`}
     >
-      <body>
-        <Header />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import About from '@/components/About/About'
+import Contact from '@/components/Contact/Contact'
+import Header from '@/components/Header/Header'
 import Projects from '@/components/Projects/Projects'
 import TechSection from '@/components/TechStack/TechStack'
 
 export default function Home() {
   return (
     <>
+      <Header />
       <main className="container mx-auto my-0">
         <About />
         <TechSection />
-        <Projects/>
+        <Projects />
       </main>
+      <Contact />
     </>
   )
 }
